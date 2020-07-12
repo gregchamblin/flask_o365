05885909ed4d0a1d2f8f605daca866d22438fe7b
Greg Chamblin <gregchamblin@outlook.com>
2020-07-12 16:59:52 -0400

    config file fix

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config(object):
-    SECRET_KEY = "mo\xf9\x83W\xc1\xb0\xd48\xd8\xca\t\x02.J\x05"
+    SECRET_KEY = os.getenv("SECRETKEY")
     
